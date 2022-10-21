074070fa44cfc489e144cf28e09471e029986573
NahuelTassone <nahuelmtassone@gmail.com>
2022-10-21 17:32:31 -0300

    Subiendo Mercado Liebre

---
+++ Desktop/DH/Mercado Liebre/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+
+const port = process.env.PORT||3001;
+app.listen(port,() => console.log('Servidor corriendo en el puerto $(port)'))
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/registro', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
